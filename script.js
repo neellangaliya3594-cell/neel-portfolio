39f62f5b5aa548d329a0c3c1b2192de3f1900987
neellangaliya3594-cell <neellangaliya3594@gmail.com>
2026-02-15 00:50:36 +0530

    Add files via upload
    Initial portfolio upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function toggleTheme() {
+    document.body.classList.toggle("dark");
+}
+
+/* Scroll Animation */
+const reveals = document.querySelectorAll(".reveal");
+
+window.addEventListener("scroll", () => {
+    reveals.forEach(el => {
+        const top = el.getBoundingClientRect().top;
+        if (top < window.innerHeight - 100) {
+            el.classList.add("active");
+        }
+    });
+});
